927b8a4ed235d1fd5e559b5160fe47699a0946e7
Dolan <dolanpritchett@gmail.com>
2025-04-25 21:41:38 -0600

    updated to subtract out_APP_c instead of prior

--- Matlab_Python_LDPC_update/Matlab_Python_LDPC/Python_version/Final_FInal.py
+++ Matlab_Python_LDPC_update/Matlab_Python_LDPC/Python_version/Final_FInal.py
@@ -246,7 +246,7 @@ def process_mimo_decoder_LDPC(H, Y, Es, EbN0):
             out_APP_m = np.array(out_APP_m, dtype=np.double)
             hard_code = (out_APP_m < 0)
             prior = out_APP_m.tolist()
-        ForMIMO = np.array(inv_recd_seq, dtype=np.double) - np.array(prior, dtype=np.double)  # Fixed subtraction
+        ForMIMO = np.array(inv_recd_seq, dtype=np.double) - out_APP_c  # Fixed subtraction
         interleavedForMIMO = interleaver(channel_interleaver_pattern, ForMIMO).tolist()
     return hard_code   
 
